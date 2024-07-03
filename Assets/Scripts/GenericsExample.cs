ab3852c8c72b803493cd5bb2f70f31d2344566cd
Camila-Giraldo <gc.camila28@gmail.com>
2024-07-03 18:02:20 -0500

    Class generic

--- Assets/Scripts/GenericsExample.cs
+++ Assets/Scripts/GenericsExample.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,17 +8,25 @@ public class GenericsExample : MonoBehaviour
     // Start is called before the first frame update
     private void Start()
     {
-        int[] intArray = CreateArray(4, 5);
-        Debug.Log(intArray.Length + " " + intArray[0] + " " + intArray[1]);
-        Debug.Log(intArray.GetType());
+        CreateArray(4, 3);
+        TestMultiGenerics(3, "3");
 
-        string[] stringArray = CreateArray("asdf", "qwerty");
-        Debug.Log(stringArray.Length + " " + stringArray[0] + " " + stringArray[1]);
-        Debug.Log(stringArray.GetType());
+        MyClass<int> myClass = new MyClass<int>();
     }
 
     private T[] CreateArray<T>(T firstElement, T secondElement)
     {
         return new T[] { firstElement, secondElement };
     }
+
+    private void TestMultiGenerics<T1, T2>(T1 t1, T2 t2)
+    {
+        Debug.Log(t1.GetType());
+        Debug.Log(t2.GetType());
+    }
+}
+
+public class MyClass<T>
+{
+    public T value;
 }
